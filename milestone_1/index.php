96c2c84a29fce5f7f8d08c0a3f3658e0f6db7a85
rolindaz <rolinda.zamboni@gmail.com>
2025-12-03 15:59:59 +0100

    Add password generation functionality and separate logic into functions.php

--- milestone_1/index.php
+++ milestone_1/index.php
@@ -2,6 +2,9 @@
 Creare un form che invii in GET la lunghezza desiderata della password. Una nostra funzione utilizzerà questo dato per generare una password casuale (composta da lettere minuscole, maiuscole, numeri e/o simboli) della lunghezza specificata, da restituire all’utente.
 Scriviamo tutta la logica ed il layout in un unico file index.php -->
 
+<!-- Milestone 2
+Verificato il corretto funzionamento del nostro codice, spostiamo la logica in un file functions.php, che includeremo poi nella pagina principale. -->
+
 <?php
 
 
---
+++ milestone_2/index.php
@@ -0,0 +1,50 @@
+<!--  Milestone 1
+Creare un form che invii in GET la lunghezza desiderata della password. Una nostra funzione utilizzerà questo dato per generare una password casuale (composta da lettere minuscole, maiuscole, numeri e/o simboli) della lunghezza specificata, da restituire all’utente.
+Scriviamo tutta la logica ed il layout in un unico file index.php -->
+
+<!-- Milestone 2
+Verificato il corretto funzionamento del nostro codice, spostiamo la logica in un file functions.php, che includeremo poi nella pagina principale. -->
+
+<?php
+require_once("./functions.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Password Generator</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+
+</head>
+<body>
+    <h1 class="text-center my-3">
+        Password Generator
+    </h1>
+    <div class="container">
+        <form action="" class="align-items-center d-flex flex-column gap-3">
+            <div>
+                <label for="password_length">
+                    Write your password's desired length (min: 5 - max: 25 characters):
+                </label>
+                <input type="number" name="password_length" id="password_length" min=5 max=25>
+            </div>
+            <button class="mx-auto" style="max-width: fit-content" type="submit">
+                Generate Password
+            </button>
+        </form>
+        <div class="my-4 text-center">
+            La tua nuova password:
+            <?php
+            $password_desired_length = 0;
+        
+        if (isset($_GET['password_length'])) {
+            $password_desired_length = (int)$_GET['password_length'];
+            echo "<strong>" . generate_password($password_desired_length) . "</strong>";
+        }
+            ?>
+        </div>
+    </div>
+</body>
+</html>
